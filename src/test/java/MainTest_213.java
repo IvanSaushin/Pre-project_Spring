9a97824a3e1f4b2422d3ac9f3c8addf4e5ce2af5
Ivan Saushin <ivan.saushin@gmail.com>
2021-02-07 23:01:34 +0400

    decided - KoscheiTheDeathless

--- src/test/java/MainTest_213.java
+++ src/test/java/MainTest_213.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
-public class MainTest {
+public class MainTest_213 {
 
     @Autowired
     private ApplicationContext applicationContext;
